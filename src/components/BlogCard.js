462fac433737db76bdfc5cca6ee5af1a4bc8b889
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-22 00:14:02 +0530

    css changes

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function BlogCard(props) {
-    
+
+    const d = new Date(props.blog.date)
     return (
         <div className="blog-card">
             <div className="d-flex between">
                 <h2>{props.blog.title}</h2>
-                <p>{props.blog.date}</p>
+                <p>{d.toDateString()}</p>
             </div>
             <p dangerouslySetInnerHTML={{ __html: props.blog.excerpt }}>{}</p>
             <p>Posted By: {props.blog.author.name}</p>
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loader() {
     return (
         <div>
-            <h1>Loader</h1>
+            <h1>Loading</h1>
         </div>
     )
 }
