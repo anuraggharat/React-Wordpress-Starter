1bc701cf8b12e4854c31a78812fd19a6d9682a21
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-03-10 13:47:06 +0530

    scroll to top added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Pagenotfound from "./pages/404";
 import About from "./pages/About";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/blog/:slug" exact component={Blog} />
-          <Route path="/about" exact component={About} />
+        <ScrollToTop>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/blog/:slug" exact component={Blog} />
+            <Route path="/about" exact component={About} />
 
-          <Route exact component={Pagenotfound} />
-        </Switch>
+            <Route exact component={Pagenotfound} />
+          </Switch>
+        </ScrollToTop>
       </BrowserRouter>
     </>
   );
